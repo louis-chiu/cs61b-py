4aa217efb2f9cde9e28d351e8dd622fd5c361fc5
Louis Chiu <louischiu@leandev.io>
2024-07-21 00:24:08 +0800

    Feat: implement IntList class
    Create an IntList class that can do follow up things:
     - Put integer type element into the IntList "first" attribute
     - Get the size of an IntList iteratively and recursively
     - Get the index of an IntList element
    
    References: Hug61B Chapter 2.1

---
+++ lists/int_list.py
@@ -0,0 +1,42 @@
+from typing import Self
+
+
+class IntList:
+    first: int
+    rest: Self | None
+
+    def __init__(self, first: int, rest: Self | None) -> None:
+        self.first = first
+        self.rest = rest
+
+    def iterativeSize(self) -> int:
+        """Iteratively get the size of the IntList"""
+        if self.rest is None:
+            return 1
+
+        count: int = 0
+        pointer: Self = self
+        while pointer is not None:
+            count += 1
+            pointer = pointer.rest
+        return count
+
+    def size(self) -> int:
+        """Recursively get the size of the IntList."""
+        if self.rest is None:
+            return 1
+
+        return 1 + self.rest.size()
+
+    def get(self, index: int) -> Self:
+        """Get the value at index of the IntList."""
+        if index >= self.size() or index < 0:
+            raise IndexError("Out of range")
+
+        count: int = 0
+        pointer: Self = self
+        while count != index:
+            count += 1
+            pointer = pointer.rest
+
+        return pointer
