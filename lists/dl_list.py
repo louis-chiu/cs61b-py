19c87f7e2879db8264aee3de42d0e74bb0e9b4ee
Louis Chiu <louis.yr.chiu@gmail.com>
2024-07-21 12:33:32 +0800

    Feat: implement DLListV2 class
    Create a DLListV2 class which is an improved version of SLList.
    It adheres to the following invariants:
     - It has a single sentinel node.
     - The first item (if it exists) is always at sentinel.next_.item.
     - The last item (if it exists) is always at sentinel.prev.item.
     - sentinel.next_ and sentinel.prev will point to the sentinel node
    itself if the DLList is empty.
     - The size_ variable always reflects the total number of items added or
    removed.
    
    It improves the following aspects:
     - Adds a prev attribute to _Node_ to make removing the last element of
    the DLList efficient.
     - Makes _Node_.item accept a generic type U so that the DLList can
    store elements of type U.
    
    References: Hug61B Chpater 2.3

--- lists/dl_list.py
+++ lists/dl_list.py
@@ -76,3 +76,79 @@ class DLList[T]:
         """Return the size of the list by using cache."""
         return self.size_
 
+
+_Node_ = TypeVar("_Node_", bound="DLListV2._Node_")
+U = TypeVar("U")
+
+
+class DLListV2[U]:
+    """
+     A class that can store elements of type U in a doubly linked list.
+     This implementation uses single sentinel node with circular.
+
+     Attributes
+     ----------
+     sentinel : _IntNode
+         A reference to a sentinel node whose prev attribute points to the last node in the DLListV2
+         and the next_ attribute points to the first node in the DLListV2.
+     size_ : int
+         The size of the DLListV2.
+     """
+    sentinel: _Node_
+    size_: int
+
+    class _Node_:
+        prev: Self | None
+        item: U | None
+        next_: Self | None
+
+        def __init__(self, item: U | None = None, prev: Self | None = None, next_: Self | None = None) -> None:
+            self.prev = prev
+            self.item = item
+            self.next_ = next_
+
+    def __init__(self, x: U | None = None) -> None:
+        """Makes the sentinel node point to itself if the DLList is empty."""
+        self.sentinel = self._Node_(None, None, None)
+        self.sentinel.prev = self.sentinel
+        self.sentinel.next_ = self.sentinel
+        self.size_ = 0
+
+        if x is not None:
+            new_node: _Node_ = self._Node_(x, None, None)
+            self.sentinel.next_ = new_node
+            self.sentinel.prev = new_node
+            self.size_ += 1
+
+    def add_first(self, x: U) -> None:
+        """Add x to the first of the list."""
+        self.sentinel.next_ = self._Node_(x, None, self.sentinel.next_)
+        self.size_ += 1
+
+    def get_first(self) -> U:
+        """Return the first element of the list."""
+        return self.sentinel.next_.item
+
+    def add_last(self, x: U) -> None:
+        """Add x to the last of the list."""
+        self.sentinel.prev = self._Node_(x, self.sentinel.prev, None)
+        self.size_ += 1
+
+    def get_last(self) -> U:
+        """Return the last element of the list."""
+        return self.sentinel.prev.item
+
+    def remove_last(self) -> None:
+        """Remove the last element of the list."""
+        if self.size_ == 0:
+            raise IndexError("The list is empty.")
+        elif self.size_ == 1:
+            self.sentinel.prev = None
+            self.sentinel.next_ = None
+        else:
+            self.sentinel.prev = self.sentinel.prev.prev
+        self.size_ -= 1
+
+    def size(self) -> int:
+        """Return the size of the list by using cache."""
+        return self.size_
